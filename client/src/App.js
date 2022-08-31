9853221eaf230564381efbb9c02dfd902e440e4e
RickLiu <rick.liu0121@gmail.com>
2022-08-31 13:24:39 -0400

    Finished styling with MUI. However api only seems to be working for the first couple of requests every time I open the project

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,14 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import AbcIcon from "@mui/icons-material/Abc";
+import ButtonGroup from "@mui/material/ButtonGroup";
+import { createTheme } from "@mui/material";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Typography from "@mui/material/Typography";
+
 function App() {
   const [word, setWord] = useState("software");
   const [associations, setAssociations] = useState(null);
@@ -17,28 +25,52 @@ function App() {
         setAssociations(body);
       });
   };
-
+  const clearTextfield = () => {
+    setWord("");
+  };
+  const theme = createTheme({
+    // typography: {
+    //   h1: {
+    //     marginTop: 5,
+    //     marginBottom: 5,
+    //   },
+    //   h6: {
+    //     marginBottom: 10,
+    //   },
+    // },
+    spacing: 8,
+  });
   return (
     <div className="app">
-      <h1>Word Associations Map</h1>
-      {/* <input value={word} onChange={(e) => setWord(e.target.value)} /> */}
+      <Typography variant="h2" sx={{ ml: 3 }}>
+        Word Association Tool
+      </Typography>
+      <Typography variant="h6" sx={{ ml: 3, mb: 2 }}>
+        The bigger the word, the strong its association with your search
+      </Typography>
       <TextField
-        style={{ diplay: "inline" }}
+        // style={{ diplay: "inline" }}
         label="Required"
         id="standard-basic"
         value={word}
         onChange={(e) => setWord(e.target.value)}
+        sx={{ mr: 2, ml: 3 }}
       >
         {word}
       </TextField>
-      <Button
-        endIcon={<AbcIcon />}
-        variant="contained"
-        color="secondary"
-        onClick={getAssociations}
-      >
-        Find Associations
-      </Button>
+      <ButtonGroup>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={getAssociations}
+          size="large"
+        >
+          Find Associations
+        </Button>
+        <Button color="secondary" onClick={clearTextfield}>
+          Clear field
+        </Button>
+      </ButtonGroup>
       {
         // Apply conditional rendering
         associations &&
@@ -46,10 +78,15 @@ function App() {
           (Object.keys(associations).length === 0 ? (
             <p>No results</p>
           ) : (
-            <div>
+            <div className="display-result">
               {/* Object.entries returns an array of the given object's string-key pair -> Nested Array */}
               {Object.entries(associations).map(([association, score]) => (
-                <span key={association}>{association}</span>
+                <span
+                  style={{ fontSize: (score * 1000) / 3 }}
+                  key={association}
+                >
+                  {association}
+                </span>
               ))}
             </div>
           ))
